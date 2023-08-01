da464da46145d5e6f36038cb4cb69ee20aa665b8
efecagrisen <efecagrisen@yahoo.com>
2023-08-01 22:14:29 +0300

    Wiring

---
+++ spring-03-Wiring/src/main/java/com/cydeo/casefactory/Dimensions.java
@@ -0,0 +1,17 @@
+package com.cydeo.casefactory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Dimensions {
+    private int width, height, depth;
+
+    public void pressPowerButton(){
+        System.out.println("Power button pressed");
+    }
+
+}
