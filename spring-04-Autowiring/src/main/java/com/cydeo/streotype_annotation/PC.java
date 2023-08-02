e57cbbd54da8fc5ed147f3acdd1c158ed369c8aa
efecagrisen <efecagrisen@yahoo.com>
2023-08-02 18:27:01 +0300

    Autowiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -4,14 +4,21 @@ import com.cydeo.streotype_annotation.casefactory.Case;
 import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import com.cydeo.streotype_annotation.motherboardfactory.Motherboard;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 public class PC {
 
+    //@Autowired
     private Case theCase;
+  //  @Autowired
     private Monitor monitor;
+   // @Autowired // field injection (not recommended)
     private Motherboard motherboard;
 
+    @Autowired // constructor ınjection
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
         this.monitor = monitor;
