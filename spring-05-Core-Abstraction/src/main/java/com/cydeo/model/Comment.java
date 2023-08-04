ebd31535373cd668c1461a6fabb7cbde09a88a7e
efecagrisen <efecagrisen@yahoo.com>
2023-08-04 07:00:02 +0300

    Abstraction

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -2,6 +2,8 @@ package com.cydeo.model;
 
 import lombok.Data;
 
+//model/entity objects never need @Component because it doesn't have any dependency, or it is not a dependency itself.
+
 @Data
 public class Comment {
 
--- spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -5,11 +5,28 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.proxy.EmailCommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import com.cydeo.repository.DBCommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
 
-    private CommentRepository commentRepository;
-    private CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+    //            interface name      local variable      : use polymorphism for loose coupling
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    /**
+     * in order to avoid any mistake regarding the autowiring developers put final to:
+     * get a warning if a constructor is forgotten to be created
+     * or
+     * to get a warning if a new field is added, and it is not included in the constructor
+     * then we fix it immediately
+     */
+
+    //we create a constructor so that spring can autowire (if there is only one constructor, no need for @Autowired annotation)
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment (Comment comment){
         // save in the DB
