efcecba33eab58498aafe0d6d8f3c1e345eae368
efecagrisen <efecagrisen@yahoo.com>
2023-08-05 22:12:42 +0300

    Spring Boot

---
+++ spring-07-SpringBoot-Demo/src/test/java/com/cydeo/spring07springbootdemo/CydeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring07springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CydeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
