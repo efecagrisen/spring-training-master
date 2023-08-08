400830bf18815bca67fd19adb396d59485a34308
efecagrisen <efecagrisen@yahoo.com>
2023-08-08 16:19:45 +0300

    Spring MVC

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CydeoApplication.class, args);
+	}
+
+}
+
+//0:57
