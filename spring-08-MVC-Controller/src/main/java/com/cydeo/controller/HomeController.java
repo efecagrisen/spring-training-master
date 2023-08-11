8cc8eb2a3151853b48c2fda214b802a4e097d5fb
efecagrisen <efecagrisen@yahoo.com>
2023-08-11 17:55:52 +0300

    MVC Controller

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(){ // method name doesn't matter
+        return "home.html";
+    }
+
+}
