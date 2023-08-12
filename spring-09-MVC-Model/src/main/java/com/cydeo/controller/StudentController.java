85fc608dd14cab5f03f6e44e03756bb1c1cf8aa1
efecagrisen <efecagrisen@yahoo.com>
2023-08-12 11:56:03 +0300

    MVC Model

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +17,23 @@ public class StudentController {
 
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(2);
+        numbers.add(16);
+
+        model.addAttribute("numbers",numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date",dt);
 
         return "student/welcome";
     }
