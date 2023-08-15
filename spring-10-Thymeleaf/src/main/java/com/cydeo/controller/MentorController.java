b29153acf5e01c3470f8792fc118c4764ed3fea2
efecagrisen <efecagrisen@yahoo.com>
2023-08-15 13:08:07 +0300

    forms

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
@@ -13,6 +16,9 @@ public class MentorController {
     @GetMapping("/register") //localhost:8080/mentor/register
 
     public String register(Model model){
+
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
 
         model.addAttribute("mentor",new Mentor());
 
