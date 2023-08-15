4b100dbcbd01d17cb4789521fb1f9b975a4e9689
efecagrisen <efecagrisen@yahoo.com>
2023-08-15 11:44:32 +0300

    forms

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +12,10 @@ public class MentorController {
 
     @GetMapping("/register") //localhost:8080/mentor/register
 
-    public String register(){
+    public String register(Model model){
+
+        model.addAttribute("mentor",new Mentor());
+
         return "mentor/mentor-register";
     }
 
