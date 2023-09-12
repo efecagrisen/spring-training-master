73cf2c9b386ec60ef68ef71a2e0d7241c68ce493
efecagrisen <efecagrisen@yahoo.com>
2023-09-12 15:55:50 +0300

    repository layer

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,9 +1,19 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Car;
+import com.cydeo.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
+
+    private final CarRepository carRepository;
+
+    public DataGenerator(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -11,7 +21,11 @@ public class DataGenerator implements CommandLineRunner {
         Car c2 = new Car("Honda","Civic");
         Car c3 = new Car("Toyota","Corolla");
 
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
 
+        //2:30
 
     }
 }
