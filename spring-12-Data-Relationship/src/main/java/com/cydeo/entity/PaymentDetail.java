43704ced92a8c9016274f7207a22d06a87e8054f
efecagrisen <efecagrisen@yahoo.com>
2023-09-13 14:13:37 +0300

    Spring Data Table Relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "paymentDetails")
+@NoArgsConstructor
+@Data
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal merchantPayoutAmount;
+    private BigDecimal commissionAmount;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
+        this.merchantPayoutAmount = merchantPayoutAmount;
+        this.commissionAmount = commissionAmount;
+        this.payoutDate = payoutDate;
+    }
+}
