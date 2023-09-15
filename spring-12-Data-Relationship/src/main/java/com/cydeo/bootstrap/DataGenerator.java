0fc7462f1f7d472f2ac83c34dbfa91029da5ec3c
efecagrisen <efecagrisen@yahoo.com>
2023-09-15 14:20:53 +0300

    Spring Data Table Relationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -2,6 +2,8 @@ package com.cydeo.bootstrap;
 
 import com.cydeo.entity.*;
 import com.cydeo.enums.Status;
+import com.cydeo.repository.CartRepository;
+import com.cydeo.repository.ItemRepository;
 import com.cydeo.repository.MerchantRepository;
 import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -16,12 +18,15 @@ public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
     private final MerchantRepository merchantRepository;
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, ItemRepository itemRepository, CartRepository cartRepository) {
         this.paymentRepository = paymentRepository;
         this.merchantRepository = merchantRepository;
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -53,7 +58,17 @@ public class DataGenerator implements CommandLineRunner {
         cart2.setItemList(Arrays.asList(item1,item2));
 
 
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+
         merchantRepository.save(merchant1);
+
 
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
