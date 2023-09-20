5170d142faa82c0c030ec55628466f027d6bb916
efecagrisen <efecagrisen@yahoo.com>
2023-09-20 16:16:29 +0300

    Derived queries practice

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,23 +9,34 @@ import org.springframework.stereotype.Component;
 public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("-----REGIONS-----");
+
         System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
-
         System.out.println("findByCountryContaining = " + regionRepository.findByCountryContaining("United"));
-
         System.out.println("findByCountryContainingOrderByRegionDesc: " + regionRepository.findByCountryContainingOrderByRegionDesc("United"));
-
         System.out.println("findTop2ByCountry: " + regionRepository.findTop2ByCountry("United States"));
-
         System.out.println("findTopByCountryContainsOrderByRegion: " + regionRepository.findTopByCountryContainsOrderByRegion("United"));
+
+        System.out.println("-----DEPARTMENTS-----");
+
+        System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision: " + departmentRepository.findByDivision("Health"));
+        System.out.println("findByDivisionContaining: " + departmentRepository.findByDivisionContaining("ics"));
+        System.out.println("findByDivisionEndsWith: " + departmentRepository.findByDivisionEndsWith("ics"));
+        System.out.println("findDistinctTopByDivisionContains" + departmentRepository.findDistinctTopByDivisionContains("Hea"));
+
+
+
 
     }
 }
--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -3,8 +3,25 @@ package com.cydeo.repository;
 import com.cydeo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department,String> {
 
+    //Display all departments in the Furniture Department
+    List<Department> findByDepartment(String department);
+
+    //Display all departments in the Health Division
+    List<Department> findByDivision(String division);
+    List<Department> findByDivisionIs(String division);
+    List<Department> findByDivisionEquals(String division);
+
+    //Display all departments with division name ends with 'ics'
+    List<Department> findByDivisionContaining(String ending);
+    List<Department> findByDivisionEndsWith(String ending);
+
+
+    //Display top 3 departments with division name includes 'Hea' without duplicates
+    List<Department> findDistinctTopByDivisionContains(String pattern);
 
 
 
