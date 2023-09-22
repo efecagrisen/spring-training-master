9e5119d4bd1228623d8fd96ced1a4f186e29e693
efecagrisen <efecagrisen@yahoo.com>
2023-09-22 10:08:48 +0300

    derived queries practice + JPQL

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CourseRepository extends JpaRepository<Course,Long> {
 
@@ -13,15 +14,17 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
     //Find all courses by category and order the entities by name
     List<Course> findByCategoryOrderByName(String category);
 
-    //0:25
-
-    //Checks if a course with the supplied name exists. Return true if exists, false otherwise
+    //Checks if a course with the supplied name exists. Return true if exists, false otherwise,
+    boolean existsByName (String name);
 
     //Returns the count of courses for the given category
+    int countByCategory(String category);
 
     //Finds all the courses that start with the given course name string
+    List<Course> findByNameStartingWith(String name);
 
     //Find all courses by category and returns a stream
+    Stream<Course> streamAllByCategory(String category);
 
 
 
