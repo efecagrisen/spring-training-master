7d24d189fa506f9d46235da7fadc0d76a211371e
efecagrisen <efecagrisen@yahoo.com>
2023-10-30 15:27:51 +0300

    Consume api with RestTemplate

--- spring-17-REST-ConsumeApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ spring-17-REST-ConsumeApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,11 +1,14 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/cydeo")
@@ -23,7 +26,29 @@ public class Consume_RestTemplate {
 
     @GetMapping
     public ResponseEntity<User[]> realAllUsers(){
-        return restTemplate.getForEntity(URI, User[].class);
+        return restTemplate.getForEntity(URI, User[].class); // getForEntity enables us to modify json body through dto classes
     }
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id")Integer id){
+        String URL = URI + "/{id}";
+
+        return restTemplate.getForObject(URL, Object.class, id); // getForObject do not need dto classes
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostFromDummyApi(){
+
+        HttpHeaders headers =new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+    }
+
 
 }
