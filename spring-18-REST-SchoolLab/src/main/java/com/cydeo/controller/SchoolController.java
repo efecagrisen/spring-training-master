78c1c522f081d39ffaeda8a4752479eeae902a76
efecagrisen <efecagrisen@yahoo.com>
2023-11-11 11:59:01 +0300

    Adding controllers

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,7 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +14,25 @@ import java.util.List;
 public class SchoolController {
 
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/teachers")
     public List<TeacherDTO> readAllTeacher(){
         List<TeacherDTO> teachers = teacherService.findAll();
         return teachers;
-
     }
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
+        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll()));
+    }
+
+
 
 
 }
